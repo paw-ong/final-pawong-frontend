02bc87152b1d60cc5b28bd6e58b007de38cd1c2b
h0725j <guswldpqj1@naver.com>
2025-05-27 16:56:17 +0900

    [refactor] import firebaseConfig

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,16 +1,6 @@
 importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js');
-
-// Firebase 구성 정보
-const firebaseConfig = {
-  apiKey: "AIzaSyCrL_4VifmlMncW15zt5ES1e2QwJM1NuVg",
-  authDomain: "pawong-e6e02.firebaseapp.com",
-  projectId: "pawong-e6e02",
-  storageBucket: "pawong-e6e02.firebasestorage.app",
-  messagingSenderId: "582136713985",
-  appId: "1:582136713985:web:fe629e7f112fa1c18cc100",
-  measurementId: "G-ETSC80ERZC"
-};
+importScripts('./firebase-config.js');
 
 firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
