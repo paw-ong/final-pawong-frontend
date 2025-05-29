3bbc8611e6f4a1c8bed303bf81a6e70ae6536048
Sungyeon <csy357@gmail.com>
2025-05-29 14:55:55 +0900

    [feat] 응답 인터셉터 개선 및 401 에러 발생 시 인증 모달 표시 기능 추가

--- src/api/client.js
+++ src/api/client.js
@@ -1,5 +1,7 @@
 // src/api/client.js
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
 axios.defaults.withCredentials = true;
 
 // CSRF 토큰 쿠키 발급 요청
@@ -25,52 +27,30 @@ const processQueue = (error, token = null) => {
   failedQueue = []
 }
 
-// 응답 에러 처리
-client.interceptors.response.use(
-    // 성공적인 응답 처리
-    response => ({
-        status: response.status,
-        data: response.data
-      }),
-    // 에러 응답 처리
-    error => {
-        const { response, config: originalRequest } = err
-        if (response?.status === 401) {
-            const code = response.data?.code
-            console.log(code)
-      
-            // 2-1) 토큰 만료
-            // if (!isRefreshing) {
-            //   isRefreshing = true
-            //   // 리프레시 토큰으로 재발급
-            //   return client
-            //     .post('/auth/refresh')
-            //     .then(refreshRes => {
-            //       isRefreshing = false
-            //       processQueue(null, refreshRes.data)  // 만약 새 토큰을 body로 받았다면
-            //       // 원래 요청 재시도
-            //       return client(originalRequest)
-            //     })
-            //     .catch(refreshErr => {
-            //       isRefreshing = false
-            //       processQueue(refreshErr, null)
-            //       // 재발급 실패 → 로그인 페이지로
-            //       window.location.href = '/login'
-            //       return Promise.reject(refreshErr)
-            //     })
-            // }
-      
-            // 이미 리프레시 중이면 큐에 대기
-            // return new Promise((resolve, reject) => {
-            //   failedQueue.push({ resolve, reject })
-            // }).then(() => client(originalRequest))
-      
-        }
-      
-    // 그 외는 그냥 reject
-    return Promise.reject(err)
-}
-)
+// 전역 이벤트를 통해 모달 표시
+const showAuthModal = () => {
+  const event = new CustomEvent('showAuthModal');
+  window.dispatchEvent(event);
+};
 
+// 응답 인터셉터
+client.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    const originalRequest = error.config;
+
+    // X-Skip-Auth-Error 헤더가 있는 경우 401 에러 처리를 건너뜁니다
+    if (error.response?.status === 401) {
+      if (!originalRequest.headers['X-Skip-Auth-Error']) {
+        console.log('401 에러 발생');
+        window.dispatchEvent(new Event('showAuthModal'));
+      }
+      return Promise.reject(error);
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default client;
