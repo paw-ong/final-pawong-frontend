ef07bebc6a7c2d2833de092fa2644fe005a8563b
Yoo Hyunseok <49493385+fiedith@users.noreply.github.com>
2025-05-29 18:18:04 +0900

    [PW-40] Firebase 관련 설정파일 workflow에 넣도록 추가 (#41)
    * [feat] firebase configuration 파일 시크릿 변수 주입
    
    * [test] import 변경
    
    * [feat] 파일을 복사할 directory 먼저 생성
    
    * [test] 파일 검증
    
    * [fix] 오타 수정
    
    * [fix] named import 사용
    
    * [test] 테스트 복구
    
    * merge develop

--- src/firebase/config.jsx
+++ src/firebase/config.jsx
@@ -2,6 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getMessaging } from "firebase/messaging";
 import firebaseConfig from './firebaseConfig';
 
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const messaging = getMessaging(app);
