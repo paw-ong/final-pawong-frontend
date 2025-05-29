264265cb0005b90d8ffef87c416fe540b92c22f6
Yoo Hyunseok <brianyhs@naver.com>
2025-05-29 18:17:01 +0900

    merge develop

--- src/firebase/config.jsx
+++ src/firebase/config.jsx
@@ -2,6 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getMessaging } from "firebase/messaging";
 import { firebaseConfig } from './firebaseConfig.js';
 
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const messaging = getMessaging(app);
